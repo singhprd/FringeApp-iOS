feacd97dffe357eaf8e9490d22b8c0399203a33e
Peter Singh <singh.prd@gmail.com>
2018-02-08 00:42:38 +0000

    Initial Commit

---
+++ FringeApp-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FringeApp-iOS
+//
+//  Created by Peter Singh on 08/02/2018.
+//  Copyright © 2018 Peter Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
